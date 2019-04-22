dc97f73a0c5fcf02cd190784fddd703ccf5e1346
Ash Wilson <smashwilson@gmail.com>
2019-04-21 20:32:48 -0400

    Verbose and quiet flags

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io"
 	"os"
@@ -12,7 +13,48 @@ import (
 )
 
 func main() {
-	if fn, ok := commands[os.Args[1]]; ok {
+	var (
+		verbose = false
+		quiet   = false
+		help    = false
+	)
+
+	const verboseDescription = "Log everything that may be logged."
+	flag.BoolVar(&verbose, "verbose", false, verboseDescription)
+	flag.BoolVar(&verbose, "v", false, verboseDescription)
+
+	const quietDescription = "Log only warnings and errors."
+	flag.BoolVar(&quiet, "quiet", false, quietDescription)
+	flag.BoolVar(&quiet, "q", false, quietDescription)
+
+	const helpDescription = "Show this message."
+	flag.BoolVar(&help, "help", false, helpDescription)
+	flag.BoolVar(&help, "h", false, helpDescription)
+
+	flag.Parse()
+
+	if verbose && quiet {
+		log.Error("-verbose and -quiet may not be provided together.")
+		writeHelp(os.Stderr, 1)
+	}
+
+	if verbose {
+		log.SetLevel(log.TraceLevel)
+	}
+	if quiet {
+		log.SetLevel(log.WarnLevel)
+	}
+
+	if help {
+		writeHelp(os.Stdout, 0)
+	}
+
+	if flag.NArg() != 1 {
+		log.Error("You must provide exactly one command.")
+		writeHelp(os.Stderr, 1)
+	}
+
+	if fn, ok := commands[flag.Arg(0)]; ok {
 		fn()
 	} else {
 		writeHelp(os.Stderr, 1)
@@ -20,13 +62,11 @@ func main() {
 }
 
 var commands = map[string]func(){
-	"-h":     help,
-	"help":   help,
-	"--help": help,
-	"init":   initialize,
-	"diff":   diff,
-	"sync":   sync,
-	"serve":  serve,
+	"help":  help,
+	"init":  initialize,
+	"diff":  diff,
+	"sync":  sync,
+	"serve": serve,
 }
 
 func help() {
