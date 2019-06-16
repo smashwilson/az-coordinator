d159fe768216fd185521f279d59e95c52d51ab06
Ash Wilson <smashwilson@gmail.com>
2019-06-16 12:10:40 -0400

    More initialization work

--- cli/init.go
+++ cli/init.go
@@ -1,13 +1,17 @@
 package cli
 
 import (
+	"fmt"
 	"io/ioutil"
 	"os"
 	"os/exec"
 	"os/user"
+	"path/filepath"
 	"strconv"
+	"strings"
 
 	log "github.com/sirupsen/logrus"
+	"github.com/smashwilson/az-coordinator/config"
 )
 
 const dbusConf = `<!DOCTYPE busconfig PUBLIC "-//freedesktop//DTD D-Bus Bus Configuration 1.0//EN"
@@ -40,8 +44,148 @@ const polkitConf = `polkit.addRule(function(action, subject) {
     }
 })`
 
+func ensureGroup(groupName string) {
+	if _, err := user.LookupGroup(groupName); err != nil {
+		if _, ok := err.(user.UnknownGroupError); ok {
+			if output, err := exec.Command("groupadd", groupName).CombinedOutput(); err != nil {
+				log.WithFields(log.Fields{
+					"err":       err,
+					"groupName": groupName,
+				}).Fatalf("Unable to create group.\n%s", output)
+			}
+		}
+
+		log.WithFields(log.Fields{
+			"err":       err,
+			"groupName": groupName,
+		}).Fatalf("Unable to search for existing group.")
+	}
+
+	log.WithField("groupName", groupName).Debug("Group already exists.")
+}
+
+func ensureUser(userName string, groupNames ...string) {
+	existing, err := user.Lookup(userName)
+	if err != nil {
+		if _, ok := err.(user.UnknownUserError); ok {
+			args := []string{"--user-group", "--no-create-home", "--shell=/bin/false"}
+			if len(groupNames) > 0 {
+				args = append(args, fmt.Sprintf("-G%s", strings.Join(groupNames, ",")))
+			}
+			args = append(args, userName)
+
+			if output, err := exec.Command("useradd", args...).CombinedOutput(); err != nil {
+				log.WithFields(log.Fields{
+					"err":      err,
+					"userName": userName,
+				}).Fatalf("Unable to create user.\n%s", output)
+			}
+		} else {
+			log.WithFields(log.Fields{
+				"err":      err,
+				"userName": userName,
+			}).Fatalf("Unable to search for existing user.")
+		}
+	} else {
+		log.WithField("userName", userName).Debug("User already exists.")
+	}
+
+	expectedGids := make(map[string]bool, len(groupNames))
+	for _, groupName := range groupNames {
+		g, err := user.LookupGroup(groupName)
+		if err != nil {
+			log.WithFields(log.Fields{
+				"err":       err,
+				"groupName": groupName,
+			}).Fatal("Unable to locate existing group.")
+		}
+
+		expectedGids[g.Gid] = true
+	}
+
+	actualGids, err := existing.GroupIds()
+	if err != nil {
+		log.WithFields(log.Fields{
+			"err":      err,
+			"userName": userName,
+		}).Fatal("Unable to enumerate groups that user belongs to.")
+	}
+
+	hasMissing := false
+	for _, actualGid := range actualGids {
+		if _, ok := expectedGids[actualGid]; ok {
+			delete(expectedGids, actualGid)
+		} else {
+			hasMissing = true
+		}
+	}
+	hasExtra := len(expectedGids) > 0
+
+	if hasMissing || hasExtra {
+		gArg := fmt.Sprintf("-G%s", strings.Join(groupNames, ","))
+		if output, err := exec.Command("usermod", gArg, userName).CombinedOutput(); err != nil {
+			log.WithFields(log.Fields{
+				"err":        err,
+				"userName":   userName,
+				"groupNames": groupNames,
+			}).Fatalf("Unable to modify groups of existing user.\n%s", output)
+		}
+
+		log.WithFields(log.Fields{
+			"userName":   userName,
+			"groupNames": groupNames,
+		}).Debug("Assigned groups to existing user.")
+	} else {
+		log.WithFields(log.Fields{
+			"userName":   userName,
+			"groupNames": groupNames,
+		}).Debug("Existing user has correct groups.")
+	}
+}
+
+func ensureDirectory(dirName string, gid int) {
+	if err := os.MkdirAll(dirName, 0770); err != nil {
+		log.WithFields(log.Fields{
+			"err":     err,
+			"dirName": dirName,
+			"gid":     gid,
+		}).Fatal("Unable to create directory.")
+	}
+	if err := os.Chmod(dirName, 0770); err != nil {
+		log.WithFields(log.Fields{
+			"err":     err,
+			"dirName": dirName,
+			"gid":     gid,
+		}).Fatal("Unable to change directory permissions.")
+	}
+	if err := os.Chown(dirName, -1, gid); err != nil {
+		log.WithFields(log.Fields{
+			"err":     err,
+			"dirName": dirName,
+			"gid":     gid,
+		}).Fatal("Unable to change directory ownership.")
+	}
+
+	log.WithFields(log.Fields{
+		"dirName": dirName,
+		"groupID": gid,
+	}).Debug("Directory exists and has proper permissions and ownership.")
+}
+
+func getGid(groupName string) int {
+	group, err := user.LookupGroup(groupName)
+	if err != nil || group == nil {
+		log.WithError(err).Fatal("Unable to locate created coordinator group.")
+	}
+	gid64, err := strconv.ParseInt(group.Gid, 10, 32)
+	if err != nil {
+		log.WithError(err).Fatal("Unable to parse coordinator group ID.")
+	}
+	return int(gid64)
+}
+
 func initialize() {
-	var r = prepare(needs{db: true})
+	var r = prepare(needs{options: true, db: true})
 
 	if _, err := r.db.Exec(`
 		CREATE TABLE IF NOT EXISTS secrets (
@@ -69,39 +213,14 @@ func initialize() {
 		log.WithError(err).Error("Unable to create secrets table.")
 	}
 
-	if _, err := user.Lookup("coordinator"); err != nil {
-		if _, ok := err.(user.UnknownUserError); ok {
-			if output, err := exec.Command("useradd", "--user-group", "--no-create-home", "--shell=/bin/false", "-Gdocker", "coordinator").CombinedOutput(); err != nil {
-				log.WithError(err).Errorf("Unable to create coordinator user.\n%s", output)
-			}
-		} else {
-			log.WithError(err).Error("Unable to locate existing system user.")
-		}
-	}
+	ensureGroup("azinfra")
+	ensureUser("coordinator", "az-infra")
 
-	group, err := user.LookupGroup("coordinator")
-	if err != nil || group == nil {
-		log.WithError(err).Fatal("Unable to locate created coordinator group.")
-	}
-	gid64, err := strconv.ParseInt(group.Gid, 10, 32)
-	if err != nil {
-		log.WithError(err).Fatal("Unable to parse coordinator group ID.")
-	}
-	gid := int(gid64)
+	gid := getGid("azinfra")
 
-	if err := os.MkdirAll("/etc/az-coordinator", 0770); err != nil {
-		log.WithError(err).Error("Unable to create coordinator configuration directory.")
-	}
-	if err := os.Chown("/etc/az-coordinator", -1, gid); err != nil {
-		log.WithError(err).Error("Unable to change coordinator configuration directory ownership.")
-	}
-
-	if err := os.MkdirAll("/etc/ssl/az/", 0770); err != nil {
-		log.WithError(err).Error("Unable to create TLS credential directory.")
-	}
-	if err := os.Chown("/etc/ssl/az", -1, gid); err != nil {
-		log.WithError(err).Error("Unable to change TLS credential directory ownership.")
-	}
+	ensureDirectory(filepath.Dir(config.DefaultOptionsPath), gid)
+	ensureDirectory("/etc/ssl/az", gid)
+	ensureDirectory("/etc/systemd/system", gid)
 
 	if err := ioutil.WriteFile("/etc/dbus-1/system.d/az-coordinator.conf", []byte(dbusConf), 0644); err != nil {
 		log.WithError(err).Error("Unable to write DBus configuration file.")
@@ -111,8 +230,33 @@ func initialize() {
 		log.WithError(err).Error("Unable to write polkit configuration file.")
 	}
 
-	if err := os.Chown("/etc/systemd/system", -1, gid); err != nil {
-		log.WithError(err).Error("Unable to change systemd unit file directory ownership.")
+	if r.options.OptionsPath != config.DefaultOptionsPath {
+		if err := os.Rename(r.options.OptionsPath, config.DefaultOptionsPath); err != nil {
+			log.WithFields(log.Fields{
+				"err":                err,
+				"optionsPath":        r.options.OptionsPath,
+				"defaultOptionsPath": config.DefaultOptionsPath,
+			}).Fatal("Unable to move options file to the default path.")
+		}
+	} else {
+		log.WithFields(log.Fields{
+			"optionsPath": r.options.OptionsPath,
+		}).Debug("Options path is already in the correct location.")
+	}
+
+	if err := os.Chown(config.DefaultOptionsPath, -1, gid); err != nil {
+		log.WithFields(log.Fields{
+			"err":                err,
+			"defaultOptionsPath": config.DefaultOptionsPath,
+			"gid":                gid,
+		}).Fatal("Unable to modify options file ownership.")
+	}
+
+	if err := os.Chmod(config.DefaultOptionsPath, 0640); err != nil {
+		log.WithFields(log.Fields{
+			"err":                err,
+			"defaultOptionsPath": config.DefaultOptionsPath,
+		}).Fatal("Unable to modify options file permissions.")
 	}
 
 	log.Info("Initialization complete.")
