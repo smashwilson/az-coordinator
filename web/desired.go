1b8331c17741b061b538da0e649ed3ff6a782e1d
Ash Wilson <smashwilson@gmail.com>
2019-06-15 21:58:36 -0400

    Desired unit update and delete

--- web/desired.go
+++ web/desired.go
@@ -4,7 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"path/filepath"
+	"regexp"
+	"strconv"
 	"strings"
 
 	log "github.com/sirupsen/logrus"
@@ -23,8 +24,34 @@ func (s Server) handleDesiredRoot(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+var desiredRx = regexp.MustCompile(`^/desired/(\d+)$`)
+
+func (s Server) handleDesired(w http.ResponseWriter, r *http.Request) {
+	rawID, ok := extractID(desiredRx, w, r)
+	if !ok {
+		return
+	}
+
+	id, err := strconv.ParseInt(rawID, 10, 32)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "Non-numeric desired unit ID (%s)", rawID)
+		return
+	}
+
+	switch r.Method {
+	case http.MethodPut:
+		s.handleUpdateDesired(w, r, int(id))
+	case http.MethodDelete:
+		s.handleDeleteDesired(w, r, int(id))
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.Write([]byte("Method not allowed"))
+	}
+}
+
 func (s Server) handleListDesired(w http.ResponseWriter, r *http.Request) {
-	session, err := state.NewSession(s.db, s.ring, s.opts.DockerAPIVersion)
+	session, err := s.newSession()
 	if err != nil {
 		log.WithError(err).Error("Unable to establish a session.")
 		w.WriteHeader(http.StatusInternalServerError)
@@ -66,7 +93,7 @@ func (s Server) handleCreateDesired(w http.ResponseWriter, r *http.Request) {
 		Schedule  string                 `json:"calendar,omitempty"`
 	}
 
-	session, err := state.NewSession(s.db, s.ring, s.opts.DockerAPIVersion)
+	session, err := s.newSession()
 	if err != nil {
 		log.WithError(err).Error("Unable to establish a session.")
 		w.WriteHeader(http.StatusInternalServerError)
@@ -84,101 +111,166 @@ func (s Server) handleCreateDesired(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	var desired state.DesiredSystemdUnit
+	builder := state.BuildDesiredUnit()
+	errs := make([]error, 0)
+	tried := func(err error) {
+		if err != nil {
+			errs = append(errs, err)
+		}
+	}
 
-	// Normalize and sanitize the request to populate desired.
+	tried(builder.Path(desiredReq.Path))
+	tried(builder.Type(desiredReq.TypeName))
+	tried(builder.Container(desiredReq.Container.ImageName, desiredReq.Container.ImageTag, desiredReq.Container.Name))
+	tried(builder.Secrets(desiredReq.Secrets, *session))
+	tried(builder.Volumes(desiredReq.Volumes))
+	tried(builder.Env(desiredReq.Env))
+	tried(builder.Ports(desiredReq.Ports))
+	tried(builder.Schedule(desiredReq.Schedule))
 
-	// Path
-	desiredReq.Path = filepath.Clean(desiredReq.Path)
-	if !strings.HasPrefix(desiredReq.Path, "/etc/systemd/system/az-") {
-		log.WithField("path", desiredReq.Path).Warn("Attempt to create desired unit file in invalid location.")
+	desired, err := builder.Build()
+	tried(err)
+
+	if len(errs) > 0 {
+		var message strings.Builder
+		message.WriteString("Invalid desired unit:\n")
+		for i, err := range errs {
+			log.WithError(err).Warn("Invalid desired unit.")
+			message.WriteString(err.Error())
+			if i != len(errs)-1 {
+				message.WriteString("\n")
+			}
+		}
+
 		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("Attempt to create desired unit with invalid path"))
+		w.Write([]byte(message.String()))
 		return
 	}
-	desired.Path = desiredReq.Path
-
-	// Type
-	if desired.Type, err = state.GetTypeWithName(desiredReq.TypeName); err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte(err.Error()))
-		return
-	}
-
-	// Container.ImageName, Container.ImageTag, and Container.Name
-	if desired.Type == state.TypeSimple || desired.Type == state.TypeOneShot {
-		if !strings.HasPrefix(desiredReq.Container.ImageName, "quay.io/smashwilson/az-") {
-			log.WithField("image-name", desiredReq.Container.ImageName).Warn("Attempt to create desired unit with invalid container image.")
-			w.WriteHeader(http.StatusBadRequest)
-			w.Write([]byte("Attempt to create desired unit with invalid container image"))
-			return
-		}
-
-		if len(desiredReq.Container.ImageTag) == 0 {
-			w.WriteHeader(http.StatusBadRequest)
-			w.Write([]byte("Attempt to create desired unit without an image tag"))
-			return
-		}
-
-		if desired.Type == state.TypeSimple && len(desiredReq.Container.Name) == 0 {
-			w.WriteHeader(http.StatusBadRequest)
-			w.Write([]byte("Attempt to create desired unit without a container name"))
-			return
-		}
-	} else {
-		if len(desiredReq.Container.ImageName) > 0 || len(desiredReq.Container.ImageTag) > 0 || len(desiredReq.Container.Name) > 0 {
-			log.WithFields(log.Fields{
-				"name":      desiredReq.Container.Name,
-				"imageName": desiredReq.Container.ImageName,
-				"imageTag":  desiredReq.Container.ImageTag,
-				"unitType":  desiredReq.TypeName,
-			}).Warn("Attempt to specify container information for unit type that does not use one")
-			w.WriteHeader(http.StatusBadRequest)
-			w.Write([]byte("Attempt to specify container information for unit type that does not use one"))
-			return
-		}
-	}
-	desired.Container.Name = desiredReq.Container.Name
-	desired.Container.ImageName = desiredReq.Container.ImageName
-	desired.Container.ImageTag = desiredReq.Container.ImageTag
-
-	// Secrets
-	if err = session.ValidateSecretKeys(desiredReq.Secrets); err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprintf(w, "Invalid secret keys: %v", err)
-		return
-	}
-	desired.Secrets = desiredReq.Secrets
-
-	// Volumes
-	var badVolumes []string
-	desired.Volumes = make(map[string]string, len(desiredReq.Volumes))
-	for hostPath, containerPath := range desiredReq.Volumes {
-		normalizedHostPath := filepath.Clean(hostPath)
-		if !strings.HasPrefix(normalizedHostPath, "/etc/ssl/az/") {
-			badVolumes = append(badVolumes, hostPath)
-		} else {
-			desired.Volumes[normalizedHostPath] = containerPath
-		}
-	}
-	if len(badVolumes) > 0 {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprintf(w, "Invalid host volumes: %v", strings.Join(badVolumes, ", "))
-		return
-	}
-
-	// Copy remaining fields as-is
-	desired.Env = desiredReq.Env
-	desired.Ports = desiredReq.Ports
-	desired.Schedule = desiredReq.Schedule
 
 	if err = desired.MakeDesired(*session); err != nil {
 		log.WithError(err).Error("Unable to serialize desired unit.")
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte("Unable to store desired unit in the database"))
+		return
 	}
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(&desired)
+}
+
+func (s Server) handleUpdateDesired(w http.ResponseWriter, r *http.Request, id int) {
+	type updateRequestContainer struct {
+		Name      string `json:"name"`
+		ImageName string `json:"image_name"`
+		ImageTag  string `json:"image_tag"`
+	}
+
+	type updateRequest struct {
+		TypeName  string                 `json:"type"`
+		Container updateRequestContainer `json:"container"`
+		Secrets   []string               `json:"secrets"`
+		Env       map[string]string      `json:"env"`
+		Ports     map[int]int            `json:"ports"`
+		Volumes   map[string]string      `json:"volumes"`
+		Schedule  string                 `json:"calendar,omitempty"`
+	}
+
+	session, err := s.newSession()
+	if err != nil {
+		log.WithError(err).Error("Unable to establish a session.")
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Unable to establish a session"))
+		return
+	}
+
+	unit, err := session.ReadDesiredUnit(id)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"err": err,
+			"id":  id,
+		}).Error("Unable to load a desired unit.")
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Something went wrong with the database"))
+		return
+	}
+
+	if unit == nil {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("Desired unit not found"))
+		return
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	decoder.DisallowUnknownFields()
+
+	var updateReq updateRequest
+	if err = decoder.Decode(&updateReq); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "Unable to parse request body as JSON: %v", err)
+		return
+	}
+
+	// Normalize and sanitize the request to modify the loaded unit.
+	builder := state.ModifyDesiredUnit(unit)
+	errs := make([]error, 0)
+	tried := func(err error) {
+		if err != nil {
+			errs = append(errs, err)
+		}
+	}
+
+	tried(builder.Type(updateReq.TypeName))
+	tried(builder.Container(updateReq.Container.ImageName, updateReq.Container.ImageTag, updateReq.Container.Name))
+	tried(builder.Secrets(updateReq.Secrets, *session))
+	tried(builder.Volumes(updateReq.Volumes))
+	tried(builder.Env(updateReq.Env))
+	tried(builder.Ports(updateReq.Ports))
+	tried(builder.Schedule(updateReq.Schedule))
+	_, err = builder.Build()
+	tried(err)
+
+	if len(errs) > 0 {
+		var message strings.Builder
+		message.WriteString("Invalid desired unit:\n")
+		for i, err := range errs {
+			log.WithError(err).Warn("Invalid desired unit.")
+			message.WriteString(err.Error())
+			if i != len(errs)-1 {
+				message.WriteString("\n")
+			}
+		}
+
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(message.String()))
+		return
+	}
+
+	if err = unit.Update(*session); err != nil {
+		log.WithError(err).Error("Unable to serialize desired unit.")
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Unable to store the updated unit in the database"))
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(unit)
+}
+
+func (s Server) handleDeleteDesired(w http.ResponseWriter, r *http.Request, id int) {
+	session, err := s.newSession()
+	if err != nil {
+		log.WithError(err).Error("Unable to establish a session.")
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Unable to establish a session"))
+		return
+	}
+
+	if err := session.UndesireUnit(id); err != nil {
+		log.WithError(err).Error("Unable to delete unit.")
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Unable to delete unit"))
+	}
+
+	w.WriteHeader(http.StatusCreated)
 }
