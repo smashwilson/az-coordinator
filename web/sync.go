8a7d3f9d9f132c4b41835d6fd7083d787e970501
Ash Wilson <smashwilson@github.com>
2019-06-17 09:53:44 -0400

    Guard syncProgress with a mutex

--- web/sync.go
+++ web/sync.go
@@ -1,6 +1,9 @@
 package web
 
 import (
+	"encoding/json"
+	"net/http"
+	"sync"
 	"time"
 
 	log "github.com/sirupsen/logrus"
@@ -13,13 +16,90 @@ type syncReport struct {
 	message string
 }
 
-type syncProgress struct {
-	reportChan chan syncReport
-	deltaChan  chan *state.Delta
-	errsChan   chan []error
+type syncReportResponse struct {
+	Timestamp int64  `json:"timestamp"`
+	Elapsed   int64  `json:"elapsed"`
+	Message   string `json:"message"`
+}
 
-	reports []syncReport
-	delta   *state.Delta
+type syncProgressResponse struct {
+	InProgress bool                 `json:"in_progress"`
+	Reports    []syncReportResponse `json:"reports"`
+	Errors     []string             `json:"errors"`
+	Delta      *state.Delta         `json:"delta"`
+}
+
+type syncProgress struct {
+	lock sync.Mutex
+
+	inProgress bool
+	reports    []syncReport
+	delta      *state.Delta
+	errs       []error
+}
+
+func (p *syncProgress) request() bool {
+	p.lock.Lock()
+	defer p.lock.Unlock()
+
+	if p.inProgress {
+		return false
+	}
+
+	p.inProgress = true
+	p.reports = make([]syncReport, 0, 10)
+	p.delta = nil
+	p.errs = make([]error, 0, 10)
+	return true
+}
+
+func (p *syncProgress) appendReport(r syncReport) {
+	p.lock.Lock()
+	defer p.lock.Unlock()
+
+	p.reports = append(p.reports, r)
+}
+
+func (p *syncProgress) setErrors(errs []error) {
+	p.lock.Lock()
+	defer p.lock.Unlock()
+
+	p.errs = errs
+	p.inProgress = false
+}
+
+func (p *syncProgress) setDelta(d *state.Delta) {
+	p.lock.Lock()
+	defer p.lock.Unlock()
+
+	p.delta = d
+	p.inProgress = false
+}
+
+func (p *syncProgress) response() syncProgressResponse {
+	p.lock.Lock()
+	defer p.lock.Unlock()
+
+	reports := make([]syncReportResponse, len(p.reports))
+	for i, r := range p.reports {
+		reports[i] = syncReportResponse{
+			Timestamp: r.ts.Unix(),
+			Elapsed:   r.elapsed.Nanoseconds() / 1000000,
+			Message:   r.message,
+		}
+	}
+
+	errors := make([]string, len(p.errs))
+	for i, e := range p.errs {
+		errors[i] = e.Error()
+	}
+
+	return syncProgressResponse{
+		InProgress: p.inProgress,
+		Reports:    reports,
+		Delta:      p.delta,
+		Errors:     errors,
+	}
 }
 
 type syncReporter struct {
@@ -40,28 +120,20 @@ func (r *syncReporter) Report(description string) {
 		message: description,
 	}
 	r.lastTs = ts
-	r.progress.reportChan <- report
-}
 
-func beginSyncReporter(server *Server) *syncReporter {
-	p := syncProgress{}
-	r := syncReporter{progress: &p}
-
-	server.currentSync = &p
-
-	return &r
+	r.progress.appendReport(report)
 }
 
 func (s *Server) performSync() {
 	reporter := state.MakeCompositeReporter(
-		beginSyncReporter(s),
+		&syncReporter{progress: s.currentSync},
 		state.LogProgressReporter{},
 	)
 
 	session, err := s.newSession()
 	if err != nil {
 		log.WithError(err).Error("Unable to establish session.")
-		s.currentSync.errsChan <- []error{err}
+		s.currentSync.setErrors([]error{err})
 		return
 	}
 
@@ -70,9 +142,48 @@ func (s *Server) performSync() {
 		for _, err := range errs {
 			log.WithError(err).Warn("Synchronization error.")
 		}
-		s.currentSync.errsChan <- errs
+		s.currentSync.setErrors(errs)
 		return
 	}
 
-	s.currentSync.deltaChan <- delta
+	s.currentSync.setDelta(delta)
+}
+
+func (s *Server) handleSync(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		s.handleGetSync(w, r)
+	case http.MethodPost:
+		s.handleCreateSync(w, r)
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.Write([]byte("Method not allowed"))
+	}
+}
+
+func (s *Server) handleGetSync(w http.ResponseWriter, r *http.Request) {
+	resp := s.currentSync.response()
+
+	w.Header().Set("Content-Type", "application/json")
+	encoder := json.NewEncoder(w)
+	if err := encoder.Encode(&resp); err != nil {
+		log.WithError(err).Error("Unable to serialize sync progress.")
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Unable to serialize sync progress as JSON"))
+		return
+	}
+}
+
+func (s *Server) handleCreateSync(w http.ResponseWriter, r *http.Request) {
+	starting := s.currentSync.request()
+	if !starting {
+		w.WriteHeader(http.StatusAccepted)
+		w.Write([]byte("Sync already in progress"))
+		return
+	}
+
+	go s.performSync()
+
+	w.WriteHeader(http.StatusAccepted)
+	w.Write([]byte("Sync started."))
 }
