1520bc0dc2b28371d4ca756ddbfb113e9cd5517f
Ash Wilson <smashwilson@github.com>
2019-06-19 11:12:17 -0400

    Only return "ok" for requests to exactly /

--- web/root.go
+++ web/root.go
@@ -3,6 +3,12 @@ package web
 import "net/http"
 
 func (s Server) handleRoot(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("Not found"))
+		return
+	}
+
 	s.cors(w, r, methodHandlerMap{
 		http.MethodGet: func() { w.Write([]byte("ok")) },
 	})
